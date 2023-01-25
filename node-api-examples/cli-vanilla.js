55f24cc985704c3b14ce814f3f720b35cc5783d5
kdorsey34 <dorseykashanna@gmail.com>
2023-01-24 21:26:17 -0500

    inclass_node_examples

--- node-api-examples/cli-vanilla.js
+++ node-api-examples/cli-vanilla.js
@@ -1,7 +1,8 @@
 const arg = process.argv[2];
+//process has different properties attached to it and also methods
 
 console.log(arg);
-
+console.log(`Hello ${arg}`);
 switch (arg) {
   case "-b":
     console.log(process.argv[3]);
--- node-api-examples/http.js
+++ node-api-examples/http.js
@@ -1,8 +1,32 @@
+// const http = require("http");
+
+// http
+//   .createServer((request, response) => { 
+//     response.writeHead(200, {"Content-Type": "text/html" })
+//     response.write("<h1>Hello World</h1>");
+//     response.end();
+
+//   })
+//   .listen(8080);
 const http = require("http");
 
-http
-  .createServer((req, res) => {
-    res.write("Hello world");
-    res.end();
-  })
-  .listen(8080);
+const server = http
+  .createServer((request, response) => { 
+    console.log(request.url); //pulls routing 
+    if(request.url === "/"){
+      response.writeHead(200, {"Content-Type": "text/html" })
+      response.write("<h1>Hello World</h1>");
+      response.end();
+    }else if(request.url === "/home"){
+      response.writeHead(200, {"Content-Type": "text/html" })
+      response.write("<h1>Hello Home</h1>");
+      response.end();
+    } else{
+      response.writeHead(404);
+      response.end();
+    }
+   });
+  server.listen(8080);
+
+  export default server;
+
